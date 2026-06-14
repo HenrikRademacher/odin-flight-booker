f8dc1fb34c7598a7d873de05104a102db32df80b
Henrik Rademacher <henrik.rademacher1@gmail.com>
2026-06-14 17:34:11 +0200

    Create seach fields for flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,6 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all.includes(:departure_airport, :arrival_airport)
+    @departures = @flights.map { |flight| flight.departure }.map { |departure| departure.strftime("%Y/%m/%d") }.uniq.sort
   end
 end
