802cf160f68ee6d945438e0361e71ae5c9a67625
Henrik Rademacher <henrik.rademacher1@gmail.com>
2026-06-14 18:23:13 +0200

    Hook up search form to index view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,19 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all.includes(:departure_airport, :arrival_airport)
-    @departures = @flights.map { |flight| flight.departure }.map { |departure| departure.strftime("%Y/%m/%d") }.uniq.sort
+    @airports = Airport.all.order(name: :asc)
+    if params[:arr] && params[:dep] && params[:day] && params[:amount]
+      @dep = Airport.find_by code: params[:dep]
+      @arr = Airport.find_by code: params[:arr]
+      @day = Date.parse params[:day]
+      @stop = @day + 1.days
+      @local_flights = Flight.includes(:departure_airport, :arrival_airport).where(departure_airport: @dep).where(arrival_airport: @arr)
+      @flights = @local_flights.where("departure >= :start AND departure <= :stop", { start: @day, stop: @stop }).order(departure: :asc)
+      puts "Filtering flights"
+    else
+      @flights = Flight.all.includes(:departure_airport, :arrival_airport)
+      puts "Got all flights"
+    end
+    @departures = Flight.all.map { |flight| flight.departure }.map { |departure| departure.strftime("%Y/%m/%d") }.uniq.sort
+    puts params
   end
 end
