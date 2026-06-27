1adee1e5a929ebb48b108fb7a2e9ced2399e4fd7
Henrik Rademacher <henrik.rademacher1@gmail.com>
2026-06-27 11:50:40 +0200

    Allow passenger amount update for booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,7 @@
 class BookingsController < ApplicationController
   def new
+    @flight = Flight.find(params[:flight])
+    @departure = @flight.departure_airport
+    @arrival = @flight.arrival_airport
   end
 end
